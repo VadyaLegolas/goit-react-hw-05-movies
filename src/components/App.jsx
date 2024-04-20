443d32372dc50a2b55eb38a34f8b09c0642fa9f0
Vlad <vadya-85@mail.ru>
2024-04-20 14:30:16 +0200

    add browser route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
+import Layout from './Layout/Layout';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    // <div>Hello</div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<h1>Home page</h1>} />
+        <Route path="movies" element={<h1>Movies</h1>} />
+        <Route path="movies/:movieId" element={<h1>Movie</h1>} />
+      </Route>
+    </Routes>
   );
 };
