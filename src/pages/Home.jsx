ad58c2cba12ed28f60dce17497c4852818c63854
Vlad <vadya-85@mail.ru>
2024-04-21 19:47:46 +0200

    add trendings array

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,30 @@
+import { useEffect, useState } from 'react'
+import {GetTrendings} from '../components/services/themoviebd-api'
 export const Home = () => {
+    const [trendings, setTrendings] = useState(null)
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState(false)
+
+    useEffect(() => {
+        const fetchTrendings = async () => {
+            try {
+                setIsLoading(true)
+                const moviesFetch = await GetTrendings()
+                // console.log(moviesFetch.results)
+                setTrendings(moviesFetch.results)
+            } catch (error) {
+                setError(error)
+            }finally {
+                setIsLoading(false)
+            }  
+        }
+         fetchTrendings().then(data=>console.log(data))
+     
+    }, [])
     
+    console.log(trendings)
     return (
-        <h1>Home</h1>
+        <h1>Trending today</h1>
+        
     )
 }
