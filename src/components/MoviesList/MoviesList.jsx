a06f0ad79e81cf8bce004b3ad5f62e7165468980
Vlad <vadya-85@mail.ru>
2024-05-04 10:41:22 +0200

    add no-images

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -4,11 +4,12 @@ function MoviesList({ list, location, path = '' }) {
   return (
     <List>
       {list.map(movie => (
+        
         <MovieLink to={`${path}${movie.id}`} state={{ from: location }}>
           <MovieItem key={movie.id}>
             {
               <MovieImg
-                src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+                src="../../images/no_image.jpg"
                 width="100"
                 alt={movie.title}
               />
