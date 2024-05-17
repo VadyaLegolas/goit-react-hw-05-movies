1e00cec773a4a9f21adaf0dcea45b721f00fd7f7
Vlad <vadya-85@mail.ru>
2024-05-17 20:31:25 +0200

    Update MoviesList.jsx

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -5,21 +5,19 @@ function MoviesList({ list, location, path = '' }) {
   return (
     <List>
       {list.map(movie => (
-        <MovieLink to={`${path}${movie.id}`} state={{ from: location }}>
-          <MovieItem key={movie.id}>
-            {
-              <MovieImg
-                src={
-                  movie.backdrop_path
-                    ? `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`
-                    : noImg
-                }
-                width="100"
-                alt={movie.title}
-              />
-            }
+        // Ключ перемещен на элемент MovieLink
+        <MovieLink key={movie.id} to={${path}${movie.id}} state={{ from: location }}>
+          <MovieItem>
+            <MovieImg
+              // Исправлена интерполяция строки
+              src={movie.backdrop_path ? `https://image.tmdb.org/t/p/w500${movie.backdrop_path}` : noImg}
+              width="100"
+              // Указан альтернативный текст для изображения
+              alt={Poster for ${movie.title}}
+            />
             {movie.title}
-            {movie.release_date ? ` (${movie.release_date.slice(0, 4)})` : ''}
+            {/* Условное отображение года выпуска */}
+            {movie.release_date && ` (${movie.release_date.slice(0, 4)})`}
           </MovieItem>
         </MovieLink>
       ))}
